ccd22896173bb145b899803d1748753ad11c69a2
BozidarGrabovcic <grabovcic93@gmail.com>
2019-10-30 11:50:10 +0100

    bookInfoCommit

--- src/main/java/com/adminportal/controller/BookController.java
+++ src/main/java/com/adminportal/controller/BookController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.adminportal.domain.Book;
@@ -53,6 +54,14 @@ public class BookController {
 		return "redirect:bookList";
 	}
 	
+	@RequestMapping("/bookInfo")
+	public String bookInfo(@RequestParam("id") Long id, Model model) {
+		Book book = bookService.findById(id);
+		model.addAttribute("book", book);
+
+		return "bookInfo";
+	}
+
 	@RequestMapping("bookList")
 	public String bookList(Model model) {
 		List<Book> bookList = bookService.findAll();
--- src/main/java/com/adminportal/repository/BookRepository.java
+++ src/main/java/com/adminportal/repository/BookRepository.java
@@ -1,9 +1,12 @@
 package com.adminportal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.adminportal.domain.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
+	Optional<Book> findById(Long id);
 }
